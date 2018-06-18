b841451180de80fef817eaf769ffc48b0c1b8c39
Rodiel Martinez <Rodielmj@gmail.com>
2018-06-17 23:50:28 -0400

    Quotes component Progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Quotes from "./Components/Quotes/QuotesComponent"
 import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "reactstrap";
@@ -8,14 +9,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <Quotes></Quotes>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to our awesome project!</h1>
-        </header>
-        <p className="App-intro">
+        </header> */}
+        {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Alert color="primary">The application has boostrap installed.. Woola!! </Alert>
+        </p> */}
+        {/* <Alert color="primary">The application has boostrap installed.. Woola!! </Alert> */}
       </div>
     );
   }
--- src/Components/Quotes/QuotesComponent.js
+++ src/Components/Quotes/QuotesComponent.js
@@ -1,49 +1,173 @@
 import React from "react";
 import { getQuote } from "./QuotesApi";
-import { Section, SectionsContainer } from "react-fullpage";
 
 class Quotes extends React.Component {
   state = {
-    quotes: []
+    quotes: [
+      {
+          "quote": "His lips caressed her ear. Best dance of my entire life.\n    \n    Lisa Kessler,\n    \n      Dance of the Heart",
+          "author": "Lisa Kessler",
+          "likes": 0,
+          "tags": [
+              "romance",
+              "military",
+              "zeus",
+              "mythology",
+              "paranormal",
+              "bodyguard",
+              "muses"
+          ],
+          "pk": 60596,
+          "image": null
+      },
+      {
+          "quote": "Youre mine, she whispered.\tAs long as youll have me.\n    \n    Lisa Kessler,\n    \n      Dance of the Heart",
+          "author": "Lisa Kessler",
+          "likes": 0,
+          "tags": [
+              "rhea",
+              "romance",
+              "military",
+              "zeus",
+              "mythology",
+              "paranormal",
+              "muses",
+              "bodyguard"
+          ],
+          "pk": 60595,
+          "image": null
+      },
+      {
+          "quote": "She danced her way right into his heart. Corny, but true.\n    \n    Lisa Kessler,\n    \n      Dance of the Heart",
+          "author": "Lisa Kessler",
+          "likes": 0,
+          "tags": [
+              "romance",
+              "military",
+              "dance",
+              "mythology",
+              "paranormal",
+              "bodyguard"
+          ],
+          "pk": 60594,
+          "image": null
+      },
+      {
+          "quote": "She lifted one arm up, covering her forehead in a mock swoon. Youre sweeping me off my feet.\tHe laughed, shocked by the sound after the shitty day theyd had. He bent his knees and cradled her in his arms, spinning her around while he drank in the magic of her giggles. Finally, he lowered her onto the bed and lay down beside her. Now youve been swept.\n    \n    Lisa Kessler,\n    \n      Dance of the Heart",
+          "author": "Lisa Kessler",
+          "likes": 0,
+          "tags": [
+              "romance",
+              "dance",
+              "mythology",
+              "paranormal-romance"
+          ],
+          "pk": 60593,
+          "image": null
+      },
+      {
+          "quote": "Tera, will you dance with me?\tShe sniffled and laughed all at once. You dont dance.\tThat was before I met you.\n    \n    Lisa Kessler,\n    \n      Dance of the Heart",
+          "author": "Lisa Kessler",
+          "likes": 0,
+          "tags": [
+              "romance",
+              "military",
+              "dance",
+              "mythology",
+              "paranormal"
+          ],
+          "pk": 60592,
+          "image": null
+      },
+      {
+          "quote": "Youre too beautiful to be worshiped in the dark.\n    \n    Lisa Kessler,\n    \n      Dance of the Heart",
+          "author": "Lisa Kessler",
+          "likes": 0,
+          "tags": [
+              "romance",
+              "military",
+              "dance",
+              "paranormal"
+          ],
+          "pk": 60591,
+          "image": null
+      },
+      {
+          "quote": "Come up when youre finished down here.\tHe raised a brow. I dont have any alarms to check up there.\tThat sexy sparkle was back in her eyes. Then I guess youll just have to check me.\n    \n    Lisa Kessler,\n    \n      Dance of the Heart",
+          "author": "Lisa Kessler",
+          "likes": 0,
+          "tags": [
+              "romance",
+              "greek-mythology",
+              "paranormal"
+          ],
+          "pk": 60590,
+          "image": null
+      },
+      {
+          "quote": "At its heart, dance is an extension of your soul braided into the music; its not a bunch of steps to be memorized. Everyone can dance.\n    \n    Lisa Kessler,\n    \n      Dance of the Heart",
+          "author": "Lisa Kessler",
+          "likes": 0,
+          "tags": [
+              "romance",
+              "mythology",
+              "paranormal"
+          ],
+          "pk": 60589,
+          "image": null
+      },
+      {
+          "quote": "Being in love is risky... the riskiest part is there are no guarantees.\n    \n    Leona Morgan,\n    \n      Back to Us",
+          "author": "Leona Morgan",
+          "likes": 0,
+          "tags": [
+              "romance",
+              "contemporary-romance",
+              "romance-novels",
+              "romance-quotes",
+              "romance-book-quotes",
+              "love-story"
+          ],
+          "pk": 60588,
+          "image": null
+      },
+      {
+          "quote": "That's why fantasies were dangerous. You could make believe all you wanted, but at the end of the day, you couldn't escape reality.\n    \n    Leona Morgan,\n    \n      Worth the Risk",
+          "author": "Leona Morgan",
+          "likes": 0,
+          "tags": [
+              "romance",
+              "contemporary-romance",
+              "love-words",
+              "true-love",
+              "romance-novels",
+              "romance-quotes",
+              "love-story"
+          ],
+          "pk": 60587,
+          "image": null
+      }
+  ]
   };
 
   componentDidMount() {
-    getQuote()
-      .then(res => {
-        const quotes = res.data.results;
-        this.setState({ quotes });
-      })
-      .catch(err => console.log(err));
+    // getQuote()
+    //   .then(res => {
+    //     const quotes = res.data.results;
+    //     this.setState({ quotes });
+    //   })
+    //   .catch(err => console.log(err));
   }
 
   render() {
-    let options = {
-      activeClass: "active", // the class that is appended to the sections links
-      anchors: [], // the anchors for each sections
-      arrowNavigation: true, // use arrow keys
-      className: "SectionContainer", // the class name for the section container
-      delay: 1000, // the scroll animation speed
-      navigation: true, // use dots navigatio
-      scrollBar: false, // use the browser default scrollbar
-      sectionClassName: "Section", // the section class name
-      sectionPaddingTop: "10px", // the section top padding
-      sectionPaddingBottom: "10px", // the section bottom padding
-      verticalAlign: false // align the content of each section vertical
-    };
-
     return (
-      //   <div className="fullpage">
-      //     {this.state.quotes.map(quotes => (
-      //       <div className="section" key={quotes.pk}>
-      //         {quotes.quote}
-      //       </div>
-      //     ))}
-      //   </div>
-      <SectionsContainer {...options}>
-        <Section>Page 1</Section>
-        <Section>Page 2</Section>
-        <Section>Page 3</Section>
-      </SectionsContainer>
+        <div className="fullpage">
+          {this.state.quotes.map(quotes => (
+            <div className="section" key={quotes.pk}>
+              {quotes.quote}
+            </div>
+          ))}
+        </div>
     );
   }
 }
