19d1d093dafebf9456a4f5933b9aa083f3413b9a
Steven Pichardo <stevepichardo@outlook.com>
2018-06-22 15:47:25 -0400

    social media share

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "reactstrap";
-import { Alert } from "reactstrap";
+import SocialMedia from "./Components/SocialMedia";
 
 class App extends Component {
+  componentDidMount() {
+    /*eslint-disable no-undef*/
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId            : '1907512692601823',
+        autoLogAppEvents : true,
+        xfbml            : true,
+        version          : 'v3.0'
+      });
+    };
+  
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+     /*eslint-disable no-undef*/
+     
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to our awesome project!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Alert color="primary">The application has boostrap installed.. Woola!! </Alert>
+        <SocialMedia quote="That's why fantasies were dangerous. You could make believe all you wanted, but at the end of the day, you couldn't escape reality.Leona Morgan,Worth the Risk"/>
       </div>
     );
   }
